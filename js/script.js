3fec5e8723bad5b4b231ce122d5b3ab9d1f180bc
Roman <104290012+RomanM0@users.noreply.github.com>
2022-06-09 00:31:34 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+myworks_btn.onclick = function(){
+    window.location.href = '/projects';
+}
+about_btn.onclick = function(){
+    window.location.href = '/about';
+}
+header_projects.onclick = function(){
+    window.location.href = '/projects';
+}
+header_about.onclick = function(){
+    window.location.href = '/about';
+}
