5805ea33506f737d3b54ed83b4c1b928dadb0693
Roman <104290012+RomanM0@users.noreply.github.com>
2022-06-11 10:54:23 +0300

    Add files via upload

---
+++ about/script.js
@@ -0,0 +1,9 @@
+header_projects.onclick = function(){
+    window.location.href = '../projects';
+}
+header_about.onclick = function(){
+    window.location.href = '../about';
+}
+header_home.onclick = function(){
+    window.location.href = '../index.html'
+}
